81715b44d967651e303cb13dfab3b9943064d922
AndySpencerReid <40076974+AndySpencerReid@users.noreply.github.com>
2018-06-26 13:51:35 -0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Set the client user's status
+client.user.setStatus('faites ^^help pour les infos')
+  .then(console.log)
+  .catch(console.error);
+
 client.login(process.env.TOKEN);
